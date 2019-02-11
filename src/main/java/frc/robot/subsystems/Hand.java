38320df5be6b31a61d4ce024afd8747f5c46f603
Dan Sheadel <tekdemo@gmail.com>
2019-02-11 02:53:44 -0800

    Add motor to hand

--- src/main/java/frc/robot/subsystems/Hand.java
+++ src/main/java/frc/robot/subsystems/Hand.java
@@ -7,6 +7,9 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -17,6 +20,11 @@ public class Hand extends Subsystem {
 
     public Solenoid leftHand = new Solenoid(2);
     public Solenoid rightHand = new Solenoid(3);
+
+    public static final boolean OPEN = false; 
+    public static final boolean CLOSE = !OPEN;
+
+    public TalonSRX motor = new TalonSRX(14);
   
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -24,11 +32,34 @@ public class Hand extends Subsystem {
     boolean deployed = false;
   
     public Hand(){
-  
       deployed = false;
       /*leftHand.set(deployed);
       rightHand.set(deployed);
       */
+
+      //We probably want a very low continuous current, to avoid causing harm to the cargo. 
+      //this lets us keep it running without much concern, as we have no sensors
+      motor.configPeakCurrentLimit(5, 10); // 35 A 
+      motor.configPeakCurrentDuration(200, 10); // 200ms
+      motor.configContinuousCurrentLimit(2, 10); // 30A
+      motor.enableCurrentLimit(true); // turn it on
+}
+
+    public void open(){
+      leftHand.set(OPEN);
+      rightHand.set(OPEN);
+    }
+
+    public void close(){
+      leftHand.set(CLOSE);
+      rightHand.set(CLOSE);
+    }
+
+    public void intake(){
+      motor.set(ControlMode.PercentOutput, 0.5);
+    }
+    public void eject(){
+      motor.set(ControlMode.PercentOutput, -0.5);
     }
 
     @Override
