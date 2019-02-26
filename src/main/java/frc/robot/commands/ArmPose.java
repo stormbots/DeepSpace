3b98a8cb69b3e6dd62b7b548269bc0fca56e18df
Stormbots Driverstation <tekdemo+driverstation@gmail.com>
2019-02-25 17:18:56 -0800

    Fix arm pose bug

--- src/main/java/frc/robot/commands/ArmPose.java
+++ src/main/java/frc/robot/commands/ArmPose.java
@@ -77,6 +77,7 @@ public class ArmPose extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.armLift.setPose(pose);
   }
 
   // Called when another command which requires one or more of the same
